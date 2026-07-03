d91b0ebcbff46f853798a627757dc4d74027a69d
Hamsa057 <24wh1a05aj@bvrithyderabad.edu.in>
2026-07-03 12:09:56 +0530

    initial commit

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const port = 3002;
 app.use(express.static("frontend"));
 
-
 app.listen(port, function(){
     console.log("Server is running at https://localhost:" + port);
 });
